a06c9cd947a5e858a782779939cd9a1e95133922
jeshrz <jeshrz@foxmail.com>
2021-03-25 15:52:24 +0800

    first commit

---
+++ newlib/libc/sys/rtt/crt0.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "syscalls.h"
+
+#define ARG_MAX  8
+#define WEAK __attribute__((weak))
+
+extern void __libc_init_array();
+extern void __libc_fini_array();
+extern int main(int argc, char *argv[]);
+
+#if defined(__CC_ARM) || defined(__CLANG_ARM)
+extern void $Super$$__cpp_initialize__aeabi_(void);
+/* we need to change the cpp_initialize order */
+WEAK void $Sub$$__cpp_initialize__aeabi_(void)
+{
+    /* empty */
+}
+#elif defined(__GNUC__) && !defined(__CS_SOURCERYGXX_MAJ__)
+/* The _init()/_fini() routines has been defined in codesourcery g++ lite */
+WEAK void _init(){}
+WEAK void _fini(){}
+
+WEAK void *__dso_handle = 0;
+WEAK void *__libc_fini = 0;
+#endif
+
+__attribute__((noreturn))
+void _start(void)
+{
+    int argc, rc;
+    char *argv[ARG_MAX];
+
+    atexit(__libc_fini_array);
+
+    __libc_init_array();
+
+    memset(argv, 0, sizeof(argv));
+    syscall_2(SYS_dlmodule_init, (long)&argc, (long)argv);
+
+    rc = main(argc, argv);
+    syscall_0(SYS_dlmodule_cleanup);
+    exit(rc);
+
+    /* !!! should not reach there !!! */
+    while(1);
+}
